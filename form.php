8a009bd36dcf5c05df314fa8fde3aff954c09d3a
Danilo Soares <danilo.ssoares20@gmail.com>
2024-12-24 19:06:33 -0300

    Initial commit

---
+++ form.php
@@ -0,0 +1,6 @@
+<?
+$em =$_GET['em'];
+$se =$_GET['se'];
+$em2 =$_GET['em2'];
+echo "$em $se $em2";
+?>
---
+++ script.js
@@ -0,0 +1,17 @@
+
+var em = document.getElementById('em')
+   var se = document.getElementById('se')
+    var se2 = document.getElementById('se2')
+
+function Enviar(){
+   
+    var se2= Number.value(se)
+document.write(em1,se2)
+
+    
+  
+}
+
+
+
+
